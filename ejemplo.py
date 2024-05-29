c4f3430af6613473209dc2747838280f5de0e5fb
aguspere <ampereira@correo.um.edu.uy>
2024-05-29 18:49:09 -0300

    Agregue una funcion para calcular el factorial de un numero

--- ejemplo.py
+++ ejemplo.py
@@ -11,7 +11,20 @@ def pedirNumero():
     else:
         print("El valor ingresado es correcto")
 
+def factorial(valor):
+    factorial = 1
+    if valor < 0:
+        print("El valor ingresaro no es correcto")
+    else:
+        while valor != 0:
+            factorial = factorial * valor
+            valor-=1
+    print("El factorial de ", valor, "es", factorial)
+
+
 if __name__=="__main__":
     funcion()
     pedirNumero()
+    numero = int(input("Ingrese un numero "))
+    factorial(numero)
 
