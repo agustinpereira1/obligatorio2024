4ea969d1913aaf2280075f0e4fd372086e1ddf94
aguspere <ampereira@correo.um.edu.uy>
2024-05-29 18:38:46 -0300

    Agregue una funcion para ver como se me guardan en el repositorio

--- ejemplo.py
+++ ejemplo.py
@@ -2,7 +2,16 @@
 def funcion():
     print("funcion que anda")
 
+def pedirNumero():
+    numero = input("Ingrese un numero ")
+    try:
+        int(numero)
+    except ValueError:
+        print("El valor ingresado no es correcto")
+    else:
+        print("El valor ingresado es correcto")
+
 if __name__=="__main__":
     funcion()
+    pedirNumero()
 
-    
